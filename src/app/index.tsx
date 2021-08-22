fcdee3deb204d0531e8c234edd58eec5ce4812d4
Kristen Jestin <kris.jestin@laposte.net>
2021-08-22 19:47:05 +0200

    feat: add redux persistence with encryption

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,7 @@ import 'intl/locale-data/jsonp/en'
 import React, { useEffect } from 'react'
 import { useColorScheme } from 'react-native'
 import { Provider as StoreProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { dark, light, mapping } from '@eva-design/eva'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
@@ -12,13 +13,12 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import { default as appTheme } from './theme.json'
 import { ThemeContext } from '@/hooks/contexts/theme.context'
 import { AppNavigator } from '@/navigation'
-import { createStore } from '@/store'
+import storer from '@/store'
 import { setupInterceptors } from '@/api/modules/client'
 
 // main
 export default () => {
-    // data
-    const store = createStore()
+    const { store, persistor } = storer()
 
     // refs
     const [theme, setTheme] = React.useState<'dark' | 'light'>(useColorScheme() || 'light')
@@ -45,7 +45,9 @@ export default () => {
             <ThemeContext.Provider value={{ theme, toggleTheme }}>
                 <ApplicationProvider mapping={mapping} theme={{ ...themes[theme], ...appTheme }}>
                     <StoreProvider store={store}>
-                        <AppNavigator />
+                        <PersistGate loading={null} persistor={persistor}>
+                            <AppNavigator />
+                        </PersistGate>
                     </StoreProvider>
                 </ApplicationProvider>
             </ThemeContext.Provider>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,37 @@
 // imports
-import { createStore as _createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
 import thunk from 'redux-thunk'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import { reducers, actions } from './modules'
+
+// config
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    timeout: 20000,
+    transforms: [
+        encryptTransform({
+            // TODO: use env variable
+            secretKey: 'my-super-secret-key',
+        }),
+    ],
+}
 
 // main
 const middleware = applyMiddleware(thunk)
 
-/**
- * Create app store.
- */
-const createStore = (data: Object = {}) => _createStore(combineReducers(reducers), data, middleware)
+const persistedReducer = persistReducer(persistConfig, combineReducers(reducers))
 
 // exports
 export type { RootStates } from './modules'
-export { createStore, actions }
+export { createStore, actions, persistedReducer }
+export default () => {
+    const store = createStore(persistedReducer, {}, middleware)
+    // TODO: use make better typing
+    /* @ts-ignore */
+    const persistor = persistStore(store)
+    return { store, persistor }
+}
